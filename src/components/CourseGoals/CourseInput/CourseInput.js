fb6f007dbe8074acdd96589e2ac1f10a462f3c13
chanuka-dsc <chanuka.dsc@gmail.com>
2021-10-19 09:49:25 +0530

    final

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+// import styled from "styled-components";
+import Button from "../../UI/Button/Button";
+import styles from "./CourseInput.module.css";
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+//   & label {
+//     color: ${(props) => (props.inValid ? "red" : "black")};
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.inValid ? "red" : "#ccc")};
+//     background: ${(props) =>
+//       props.inValid ? "rgb(240, 217, 215)" : "transparent"};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [valid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+    } else {
+      props.onAddGoal(enteredValue);
+    }
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={styles["form-control"]}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
